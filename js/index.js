e46a20f944cb162acfb412636c5aef747e82c50f
Thomas Spencer <t.spencer1990@hotmail.com>
2016-09-14 15:58:54 -0400

    Added javascript to allow for X and O placement on the board.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
 $(function(){
 
+  var turn = 0;
+
   $('.square').on('click', function(){
-    $(this).text('X')
+    var self = $(this);
+
+    if (self.text() === "") {
+      if (turn % 2 === 0) {
+        self.text('X')
+        turn++;
+      }else {
+        self.text('O')
+        turn++;
+      }
+    } else {
+      alert("Nice try, you cheaty cheater")
+    }
+
+
   })
 
 })
